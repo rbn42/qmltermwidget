0bc252a5dd44ad7dd481ffbce5d6f77629cfa00a
rbn42 <bl100@students.waikato.ac.nz>
2016-07-13 08:24:23 +1200

    make double width charactors blink correctly

--- lib/TerminalDisplay.cpp
+++ lib/TerminalDisplay.cpp
@@ -1620,7 +1620,7 @@ QRect TerminalDisplay::imageToWidget(const QRect& imageArea) const
 
 void TerminalDisplay::updateCursor()
 {
-  QRect cursorRect = imageToWidget( QRect(cursorPosition(),QSize(2,2)) ); 
+  QRect cursorRect = imageToWidget( QRect(cursorPosition(),QSize(2,1)) ); 
   update(cursorRect);
 }
 
